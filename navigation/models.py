e4832a6dc7f3f12f02cba96333e9f2aeda33e38c
CraySparkc <roman2910z@yandex.ru>
2020-09-26 06:19:10 +0700

    Первое создание моделей

--- navigation/models.py
+++ navigation/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Place(models.Model):
+    fond = models.CharField(max_length='30', verbose_name='Фонд')
+    cab_number = models.IntegerField(verbose_name='Номер кабинета')
+    cab_name = models.CharField(max_length=50, verbose_name='Название отдела')
--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from navigation.models import Place
+
 
 # Create your models here.
+
+class Autor(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Автор')
+
+    class Meta:
+        verbose_name = "Автор"
+        verbose_name_plural = "Авторы"
+
+    def __str__(self):
+        return self.name
+
+
+class Gener(models.Model):
+    name = models.CharField(max_length=40, verbose_name='Название жанра')
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Название типа')
+
+    class Meta:
+        verbose_name = 'Тип ресурса'
+        verbose_name_plural = 'Типы ресурса'
+
+    def __str__(self):
+        return self.name
+
+
+class Stack(models.Model):
+    place = models.ForeignKey(Place)
+
+    class Meta:
+        verbose_name = 'Стеллаж'
+        verbose_name_plural = 'Стеллажи'
+
+    def __str__(self):
+        return self.place.cab_name
+
+
+class Resource(models.Model):
+    inv = models.CharField(max_length=20, verbose_name='ИНВ')
+    name = models.CharField(max_length=50, verbose_name='Название')
+    desk = models.TextField(verbose_name="Описание")
+    publishing = models.CharField(max_length=35, verbose_name='Издатель')
+    autor = models.ForeignKey(Autor)
+    gener = models.ForeignKey(Gener)
+    date = models.DateField(verbose_name='Дата публикации')
+    type = models.ForeignKey(Type)
+    stack = models.ForeignKey(Stack)
+    photo = models.ImageField(upload_to='photo_resource', blank=True)
+    mark = models.CharField(max_length=100, verbose_name='mark')
+
+
+
