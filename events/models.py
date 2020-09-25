2f3d92356cda8bfe6b254e46dbec88b2afe307e2
Denis <dany.sssok@icloud.com>
2020-09-26 06:23:39 +0700

    upp 1

--- events/models.py
+++ events/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from navigation.models import Place
 
-# Create your models here.
+class event(models.model):
+    name=models.CharField('Название' , max_length=50)
+    desc=models.TextField('Описание')
+    date=models.DateTimeField('Дата публикации' , auto_created=False)
+    theme=models.ForeignKey(theme)
+    place=models.ForeignKey(Place)
+    organizer=models.CharField('Организатор' , max_length=100)
+    photo=models.ImageField('Фото' , upload_to='photo_event' , blank=True)
+
+class theme(models.model):
+    theme=models.CharField('Тема' , auto_created=50)
+
