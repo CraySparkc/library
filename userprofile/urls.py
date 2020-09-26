9558c2fc7e17a1405f61f9cb17825713352588b1
CraySparkc <roman2910z@yandex.ru>
2020-09-26 19:32:18 +0700

    Пошла жара

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -3,4 +3,6 @@ from userprofile import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
+    path('logout/', views.user_logout, name="logout"),
+    path('login/', views.user_login, name="login")
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.views import logout
 from django.shortcuts import redirect
+from django.urls import reverse
+
+from navigation import views
 
 # Create your views here.
 
@@ -9,6 +13,15 @@ def profile(request):
     return render(request, 'userprofile/profile.html', context={})
 
 
-def logout(request):
+def user_logout(request):
     logout(request)
-    return redirect('views.index')
+    return HttpResponseRedirect(reverse('index'))
+
+def user_login(request):
+    if request.method == 'POST':
+        is_staff = request.POST.get('is-staff')
+        username = request.POST.get('username')
+    else:
+        return HttpResponseRedirect(reverse('register'))
+
+
